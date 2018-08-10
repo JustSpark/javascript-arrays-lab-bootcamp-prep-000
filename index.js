c08e9995557d5b97c8694369c664b0f5a71453a8
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-10 23:24:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push('Ralph');
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift('Bob');
+}
+function destructivelyRemoveLastKitten(name) {
+  return kittens.shift('Milo','Otis');
+}
