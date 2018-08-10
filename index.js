6670db9e308a106e2a86ebdc3b6a58673b3aba9b
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-10 23:35:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function appendKitten(name) {
   return kittens.concat('Broom');
 }
 function prependKitten(name) {
-  return kittens.splice(0, 1);
+  return kittens.splice(0, 'Arnold', 1);
 }
 function removeLastKitten(array) {
 return kittens.slice(0, kittens.length - 1);
